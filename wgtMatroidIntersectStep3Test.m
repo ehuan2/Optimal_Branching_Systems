91e8878b93d8d0e61bcbd43f6f9d097b6fb5fb77
Eric Huang <eric.haoran.huang@gmail.com>
2022-05-25 00:01:28 -0400

    Step 3 test working

---
+++ wgtMatroidIntersectStep3Test.m
@@ -0,0 +1,7 @@
+result = 1;
+% test example:
+[C1 C2] = wgtMatroidIntersectStep2(3, [0 0 0], {{}, {1}, {2}, {1, 2}}, {{}, {1}, {2}, {3}});
+[A1, A2, S, T] = wgtMatroidIntersectStep3(3, [0 0 0], {{}, {1}, {2}, {1, 2}}, {{}, {1}, {2}, {3}}, C1, C2);
+
+result = isequal(A1, {}) && isequal(A2, {}) && isequal(S, [1 1 0]) && isequal(T, [1 1 1]) && result;
+
