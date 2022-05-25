b8d15fc4e8e4796bb34727f82eb0d7d5fd6557d0
Eric Huang <eric.haoran.huang@gmail.com>
2022-05-24 23:53:59 -0400

    Added in step 2 tests

--- wgtMatroidIntersectStep2Test.m
+++ wgtMatroidIntersectStep2Test.m
@@ -5,5 +5,14 @@ result = 1;
 [C1 C2] = wgtMatroidIntersectStep2(2, [0 0], {{}}, {{}, {1}, {2}});
 result = isequal(C1, {{[0 0], 1, [1 0]}, {[0 0], 2, [0 1]}}) && isequal(C2, {{[0 0], 1, [0 0]}, {[0 0], 2, [0 0]}}) && result;
 
+% test example:
+[C1 C2] = wgtMatroidIntersectStep2(3, [0 0 0], {{}, {1}, {2}, {1, 2}}, {{}, {1}, {2}, {3}});
+
+result = isequal(C1, {
+    {[0 0 0], 1, [0 0 0]}, {[0 0 0], 2, [0 0 0]}, {[0 0 0], 3, [0 0 1]}
+  }) && isequal(C2, {
+    {[0 0 0], 1, [0 0 0]}, {[0 0 0], 2, [0 0 0]}, {[0 0 0], 3, [0 0 0]}
+  }) && result;
+
 % result = isequal(wgtMatroidIntersectStep2(), []) && result;
 % result = isequal(wgtMatroidIntersectStep2(), []) && result;
