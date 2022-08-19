c2cc483a8e5ffad8b48583e536184c5fa8d38c01
Eric Huang <eric.haoran.huang@gmail.com>
2022-08-19 08:03:04 -0400

    Add in partially working brute force

--- M2/brute-force/M2BruteForceTest.m
+++ M2/brute-force/M2BruteForceTest.m
@@ -6,14 +6,14 @@ p = SetPartition({2, 3, 5, 7}, 3);
 % partitions 6 different times! (should work!)
 
 % the following results in an error. However, we know that if roots > edges
-% we can of course partition it (each root can take an edge, as a forest)
+% we can of course partition it (each root can take an e    dge, as a forest)
 % p2 = SetPartition({2, 3, 4}, 4);
 
 % Testing M2 Brute Force ---------------------------------------------------------------------------
 % 3 nodes, 1 roots -- should have three as a solution ({1, 2}, {1, 3}, {2, 3})
 G = [0 1 1; 0 0 1; 0 0 0];
-M2 = calculateM2BruteForce(G, 1);
+M2 = calculateM2BruteForce(G, 1)
 
 % 3 nodes, 2 roots -- should have all as a solution ({1, 2, 3})
-M2_two_roots = calculateM2BruteForce(G, 2);
+M2_two_roots = calculateM2BruteForce(G, 2)
 
--- M2/brute-force/calculateM2BruteForce.m
+++ M2/brute-force/calculateM2BruteForce.m
@@ -1,81 +1,79 @@
 % this function takes a graph G and k partitions, and produces M2 through brute force
 function [M2] = calculateM2BruteForce(G, k)
   % G is fed in as an n x n adjacency matrix
-
-  % strategy:
-  % Generate all k-partitions of the set using all the edges
-  % Step 1: let's get the number of nodes, and generate the k-partitions for it
   numEdges = numberOfEdges(G);
-  kPartitions = SetPartition(numEdges, k);
-
-  M2 = {};
-  M2Counter = 1;
+  allEdges = CellArrayOfNEdges(numEdges);
 
   % transforms adjancency matrix G to an incidence matrix A
   [A, transformedG] = adjacencyToIncidenceMatrix(G);
 
-  % Step 2: For each group in kPartitions, check if each partition of the group is a forest
-  % if it is a forest, add it to the basis, M2
-  for i = 1:size(kPartitions, 1)
-    group = kPartitions{i};
-    isGroupMadeUpOfForests = 1;
-    % then, go through each partition in the group, and remove each column that's not in the partition
-    for j = 1:size(group, 2)
-      partition = group{j};
-      aRemoveEdgesFromPartition = removeNonPartitionEdges(A, partition);
-      if isForest(aRemoveEdgesFromPartition) == 0
-        % if it's not a forest, then set isGroupMadeOfForests to false, and break
-        isGroupMadeUpOfForests = 0;
+  M2 = {};
+  M2Counter = 1;
+
+  % this function takes in an edgeset and sees if it is k forest-partitionable
+  function edgeSetPartitionable = isEdgesetKForestPartitionable(edgeset)
+    % strategy:
+    % Generate all k-partitions of the set using all the edges
+    % Step 1: let's get the number of nodes, and generate the k-partitions for it
+    kPartitions = SetPartition(edgeset, k);
+
+    % assume it's not partitionable
+    edgeSetPartitionable = 0;
+
+    % Step 2: For each group in kPartitions, check if each partition of the group is a forest
+    % if it is a forest, add it to the basis, M2
+    for i = 1:size(kPartitions, 1)
+      group = kPartitions{i};
+      isGroupMadeUpOfForests = 1;
+      % then, go through each partition in the group, and remove each column that's not in the partition
+      for j = 1:size(group, 2)
+        partition = group{j};
+        aRemoveEdgesFromPartition = removeNonPartitionEdges(A, partition);
+        if isForest(aRemoveEdgesFromPartition) == 0
+          % if it's not a forest, then set isGroupMadeOfForests to false, and break
+          isGroupMadeUpOfForests = 0;
+          break;
+        end
+      end
+
+      if isGroupMadeUpOfForests == 1
+        % if it is a forest, then break, and say it's partitionable
+        edgeSetPartitionable = 1;
         break;
       end
     end
+  end
 
-    if isGroupMadeUpOfForests == 1
-      % if it is a forest, then break
+  % Step 3: Create a cell array of all the edges, and then choose numEdges of them 
+  while numEdges > 0
+    allNumEdgesets = nchoosek(allEdges, numEdges);
+    for i = 1:size(allNumEdgesets, 1)
+      edgeset = allNumEdgesets(i,:);
+      if isEdgesetKForestPartitionable(edgeset) == 1
+        M2{M2Counter} = edgeset;
+        M2Counter = M2Counter + 1;
+      end
+    end
+
+    if M2Counter == 1
+      % ie there's no added edgeset
+      numEdges = numEdges - 1;
+    else
       break;
     end
   end
 
-  % Step 3: If M2 is empty (ie M2Counter is 1), then recurse on the edgeset removing an edge
-  M2 = kPartitions;
 end
 
 % evaluates whether or not incidence matrix A is a forest
 function forestBool = isForest(A)
-  % first, calculate the g2rref
-  [Arref, M, N, rank] = g2rref(A);
-
-  % todo: check if this actually tells if it's a forest or not...
-  % now, get all the leading ones, and check the non-leading ones columns (after the last leading one)
-  % and see if it's all 0's. if it's not all zeroes, then it's not a forest
-
-  % assume it's a forest first
-  forestBool = 1;
-
-  columns = findLeadingOneColumns(Arref);
-  lastLeadingOneIndex = 0;
-  for i = 1:size(columns, 2)
-    if columns(1, i) == 1
-      lastLeadingOneIndex = i;
-    end
-  end
-
-  Arref
-  columns
-  lastLeadingOneIndex
-
-  for i = (lastLeadingOneIndex + 1):size(columns, 2)
-    % now, check if it's all zeroes in jth column
-    for j = 1:size(Arref, 1) 
-      if Arref(j, i) ~= 0
-        forestBool = 0;
-        break;
-      end
-    end
-
-    if forestBool == 0
-      break;
-    end
+  % first, check if it's just a n x 1 matrix (then it's a forest, just a single edge)
+  if size(A, 2) == 1
+    forestBool = 1;
+  else
+    % first, calculate the g2rref
+    [Arref, M, N, rank] = g2rref(A);
+    forestBool = (rank == size(A, 2)); % check if the rank if the number of columns we have
   end
 end
 
@@ -85,17 +83,16 @@ function edgesRemoved = removeNonPartitionEdges(A, partition)
   % strategy: transform the edge cell to a 1 x n matrix and then go through that matrix 
   partitionEdges = zeros(1, size(A, 2));
   for i = 1:size(partition, 2)
-    partitionEdges(1, partition(i)) = 1;
+    partitionEdges(1, partition{i}) = 1;
   end
 
-  edgesRemoved = A;
+  edgesRemoved = [];
+
   for i = 1:size(partitionEdges, 2)
-    % loop over it all, if it's a 0 (ie not in partition), then remove the entire column (ie just set all to 0)
-    if partitionEdges(i) == 0
-      % remove the entire ith column
-      for j = 1:size(A, 1)
-        edgesRemoved(j, i) = 0;
-      end
+    % loop over it all, if it's a 1 (ie in partition), then append the entire column to the matrix
+    if partitionEdges(i) == 1
+      % append the column to the end of edges removed (should work since in order)
+      edgesRemoved = [edgesRemoved A(:,i)];
     end
   end
 end
